a48120d62e3fc25cdbd4790d38d7d28fc06542ab
Denisa Haka <denisa.haka@ymail.com>
2021-11-07 00:01:08 +0000

    updated footer adding new links

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Dictionary defaultWord="sea"/>
       </main>
       <footer className="text-center mt-5"><small >Coded by <a href="https://github.com/Deni-ux/dictionary-project" target="_blank" rel="noreferrer">
-        Denisa Haka</a> </small></footer>
+        Denisa Haka</a> and is  <a href="https://github.com/Deni-ux/dictionary-project" target="_blank" rel="noreferrer">open-sourced in Github</a> and deployed on  <a href="https://dictionary-react-project.netlify.app/" target="_blank" rel="noreferrer">Netlify </a></small></footer>
     </div></div> 
   );
 }
